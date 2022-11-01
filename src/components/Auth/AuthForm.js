d55141c4f81fde17d3f824b41e936ab9914adfb8
Irene Riportella <riportellairene@gmail.com>
2022-11-01 19:41:40 +0100

    stored token and remaing time in localStorage

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -63,7 +63,9 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authCtx.login(data.idToken);
+        const expirationTime = new Date(new Date().getTime() + +data.expiresIn * 1000);
+
+        authCtx.login(data.idToken, expirationTime.toString());
         history.replace('/');
       })
       .catch((err) => {
